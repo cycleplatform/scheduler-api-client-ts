14fc88468062b95a145c171bb6439d9be7dfeb05
Alexander Mattoni <5110855+mattoni@users.noreply.github.com>
2024-03-29 13:46:10 -0700

    create scheduler ts client

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import createClient, { type Middleware } from "openapi-fetch";
+import type { paths } from "./generated/types";
+
+export function getClient({
+  accessToken,
+  baseUrl = "http://env-scheduler",
+}: {
+  baseUrl?: string;
+  accessToken: string;
+}) {
+  const client = createClient<paths>({ baseUrl });
+
+  const authMiddleware: Middleware = {
+    async onRequest(req) {
+      req.headers.set("X-CYCLE-ACCESS-KEY", accessToken);
+      return req;
+    },
+  };
+
+  client.use(authMiddleware);
+  return client;
+}
---
+++ tests/setup.ts
@@ -0,0 +1,51 @@
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { setupServer } from "msw/node";
+import { HttpResponse, http } from "msw";
+
+export const restHandlers = [
+  http.post(
+    "http://env-scheduler/v1/functions/containerId/claim",
+    ({ request }) => {
+      if (request.headers.get("X-CYCLE-ACCESS-KEY") !== "accessToken") {
+        throw new HttpResponse(null, { status: 403 });
+      }
+
+      return HttpResponse.json({
+        data: {
+          instance_id: "651586fca6078e98982dbd90",
+          environment: {
+            id: "651586fca6078e98982dbd90",
+            network_subnet: "string",
+            subnet: "string",
+            ipv6: {
+              ip: "fd00::21:0:0:0",
+              cidr: "fd00::21:0:0:0/96",
+            },
+            legacy: {
+              host: 0,
+              subnet: 0,
+              ipv4: {
+                ip: "fd00::21:0:0:0",
+                cidr: "fd00::21:0:0:0/96",
+              },
+            },
+            mac_addr: "string",
+            vxlan_tag: 0,
+          },
+          token: "string",
+        },
+      });
+    }
+  ),
+];
+
+const server = setupServer(...restHandlers);
+
+// Start server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+
+//  Close server after all tests
+afterAll(() => server.close());
+
+// Reset handlers after each test `important for test isolation`
+afterEach(() => server.resetHandlers());
