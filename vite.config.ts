c15665146c733a89a768344b2cbea5d3262b3637
Alexander Mattoni <5110855+mattoni@users.noreply.github.com>
2024-03-29 14:39:05 -0700

    scheduler client

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "./src/index.ts"),
-      name: "Cycle API Client",
+      name: "Cycle Scheduler API Client",
       // the proper extensions will be added
       fileName: "index",
     },
